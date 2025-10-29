3f602bac54a8a53832fd62853ef72f687b091511
Beyley Cardellio <ep1cm1n10n123@gmail.com>
2025-10-29 00:34:24 -0700

    d/psvr2: Exit libusb context after closing device
    We also disable USB device close due to a libusb bug. This shouldn't
    leak resources.
    
    Part-of: <https://gitlab.freedesktop.org/monado/monado/-/merge_requests/2607>

--- src/xrt/drivers/psvr2/psvr2.c
+++ src/xrt/drivers/psvr2/psvr2.c
@@ -199,19 +199,24 @@ psvr2_hmd_destroy(struct xrt_device *xdev)
 	os_thread_helper_lock(&hmd->usb_thread);
 	hmd->usb_complete = 1;
 	os_thread_helper_unlock(&hmd->usb_thread);
-	os_thread_helper_stop_and_wait(&hmd->usb_thread);
+	os_thread_helper_destroy(&hmd->usb_thread);
 
 	psvr2_usb_destroy(hmd);
 
-	if (hmd->dev != NULL) {
-		libusb_close(hmd->dev);
+	// @note We appear to be hitting a bug in libusb, so this is commented out
+	//       see: https://github.com/libusb/libusb/issues/1605
+	// if (hmd->dev != NULL) {
+	// 	libusb_close(hmd->dev);
+	// }
+
+	if (hmd->ctx != NULL) {
+		libusb_exit(hmd->ctx);
 	}
 
 	// Remove the variable tracking.
 	u_var_remove_root(hmd);
 
 	m_relation_history_destroy(&hmd->relation_history);
-	os_thread_helper_destroy(&hmd->usb_thread);
 	os_mutex_destroy(&hmd->data_lock);
 	u_device_free(&hmd->base);
 }
